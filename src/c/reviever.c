d256993414755ee5dd54d5db8003b29af68aef8a
simonlarsson555 <simon.larsson.555@gmail.com>
2024-03-28 09:27:38 +0100

    comms implementation and documentation added

--- src/c/reviever.c
+++ src/c/reviever.c
@@ -13,6 +13,16 @@
 #define SHM_REF_SIZE sizeof(struct refGen_monitor)
 #define PERMS 0666
 
+/**
+ * @brief Sets inner parameters of controller. Should only be called by python script
+ * @param K: double value K
+ * @param Ti: double value Ti
+ * @param Tr: double value Tr
+ * @param Beta: double value Beta
+ * @param H: double value H
+ * @param integratorOn: int value IntegratorOn. 1 - on, 0- off
+ * @return 0 if no error was encountered, otherwise non-0
+ */
 int setInnerParameters(double K, double Ti, double Tr, double Beta, double H, int integratorOn)
 {
     key_t key = ftok("/tmp", 'I');
@@ -43,8 +53,19 @@ int setInnerParameters(double K, double Ti, double Tr, double Beta, double H, in
         perror("Error, shmdt: ");
         return 1;
     }
+    return 0;
 }
 
+/**
+ * @brief Gets inner parameters of controller. Should only be called by python script
+ * @param K: pointer to double to get value of K
+ * @param Ti: pointer to double to get value of Ti
+ * @param Tr: pointer to double to get value of Tr
+ * @param Beta: pointer to double to get value of Beta
+ * @param H: pointer to double to get value of H
+ * @param integratorOn: pointer to int to get value of integratorOn. 1 - on, 0- off
+ * @return 0 if no error was encountered, otherwise non-0
+ */
 int getInnerParameters(double *K, double *Ti, double *Tr, double *Beta, double *H, int *integratorOn)
 {
     key_t key = ftok("/tmp", 'I');
@@ -75,8 +96,21 @@ int getInnerParameters(double *K, double *Ti, double *Tr, double *Beta, double *
         perror("Error, shmdt: ");
         return 1;
     }
+    return 0;
 }
 
+/**
+ * @brief Sets outer parameters of controller. Should only be called by python script
+ * @param K: double value K
+ * @param Ti: double value Ti
+ * @param Td: double value Td
+ * @param Tr: double value Tr
+ * @param N: double value N
+ * @param Beta: double value Beta
+ * @param H: double value H
+ * @param integratorOn: int value IntegratorOn. 1 - on, 0- off
+ * @return 0 if no error was encountered, otherwise non-0
+ */
 int setOuterParameters(double K, double Ti, double Td, double Tr, double N, double Beta, double H, int integratorOn)
 {
     key_t key = ftok("/tmp", 'D');
@@ -110,8 +144,21 @@ int setOuterParameters(double K, double Ti, double Td, double Tr, double N, doub
         perror("Error, shmdt: ");
         return 1;
     }
+    return 0;
 }
 
+/**
+ * @brief Gets outer parameters of controller. Should only be called by python script
+ * @param K: pointer to double to get value of K
+ * @param Ti: pointer to double to get value of Ti
+ * @param Td: pointer to double to get value of Td
+ * @param Tr: pointer to double to get value of Tr
+ * @param N: pointer to double to get value of N
+ * @param Beta: pointer to double to get value of Beta
+ * @param H: pointer to double to get value of H
+ * @param integratorOn: pointer to int to get value of integratorOn. 1 - on, 0- off
+ * @return 0 if no error was encountered, otherwise non-0
+ */
 int getOuterParameters(double *K, double *Ti, double *Td, double *Tr, double *N, double *Beta, double *H, int *integratorOn)
 {
     key_t key = ftok("/tmp", 'D');
@@ -144,8 +191,14 @@ int getOuterParameters(double *K, double *Ti, double *Td, double *Tr, double *N,
         perror("Error, shmdt: ");
         return 1;
     }
+    return 0;
 }
 
+/**
+ * @brief Sets mode of controller. 0 = OFF, 1 = BEAM, 2 = BALL. Should only be called by python script
+ * @param mode: int indication mode to set to. 0 = OFF, 1 = BEAM, 2 = BALL
+ * @return 0 if no error was encountered, otherwise non-0
+ */
 int setMode(int mode)
 {
 
@@ -174,8 +227,14 @@ int setMode(int mode)
         perror("Error, shmdt: ");
         return 1;
     }
+    return 0;
 }
 
+/**
+ * @brief Sets reference value. Should only be called by python script
+ * @param ref: double value of new reference
+ * @return 0 if no error was encountered, otherwise non-0
+ */
 int setRef(double ref)
 {
     key_t key = ftok("/tmp", 'R');
@@ -203,17 +262,43 @@ int setRef(double ref)
         perror("Error, shmdt: ");
         return 1;
     }
+    return 0;
 }
 
+/**
+ * @brief Gets latest control data. Should only be called by python script
+ * @param x: pointer to double to get value of x
+ * @param u: pointer to double to get value of u
+ * @return 0 if no error was encountered, otherwise non-0
+ */
 int getControlData(double *x, double *u)
 {
     *x = 1;//get value from somewhere shared memory
     *u = 1;//get value from somewhere shared memory
+    return 0;
 }
 
+/**
+ * @brief Gets latest measurement data. Should only be called by python script.
+ * @param x: pointer to double to get value of x
+ * @param yRef: pointer to double to get value of yRef
+ * @param y: pointer to double to get value of y
+ * @return 0 if no error was encountered, otherwise non-0
+ */
 int getMeasurementData(double *x, double *yRef, double *y)
 {
     *x = 1;//get value from somewhere shared memory
     *yRef = 1;//get value from somewhere shared memory
     *y = 1;//get value from somewhere shared memory
+    return 0;
+}
+
+/**
+ * @brief shuts down regulation. Does nothing yet
+ * @return 0 if no error was encountered, otherwise non-0
+ */
+int shutDown()
+{
+    //shutdown
+    return 0;
 }
--- src/python/comms.py
+++ src/python/comms.py
@@ -1,41 +1,199 @@
 import ctypes
 
-lib = ctypes.CDLL('../../bin/reciever.so')
+comms_lib = ctypes.CDLL('../../bin/reciever.so')
 
-#fix these functions so they have correct path
-def setInnerParameters(beta, H, integratorOn, K, Ti, Tr):
-    path = "./comms/setInnerParameters"
-    data = [beta, H, integratorOn, K, Ti, Tr]
-    write(path, data)
 
-def setOuterParameters(beta, H, integratorOn, K, N, Td, Ti, Tr):
-    path = "./comms/setOuterParameters"
-    data = [beta, H, integratorOn, K, N, Td, Ti, Tr]
-    write(path, data)
+def setInnerParameters(K, Ti, Tr, beta, H, integratorOn):
+    """
+    Sets parameters for inner controller.
 
-#values are OFF = 0, BEAM = 1, BALL = 2
+    Args:
+        K (double): K
+        Ti (double): Ti
+        Tr (double): Tr
+        beta (double): beta
+        H (double): H
+        integratorOn (int): 1 if integrator is on or 0 if not (needs to be int for c reasons)
+
+        Returns:
+        int: 0 if sucessfull, non-0 of not 
+    """
+    setInnerParameters_c = comms_lib.setInnerParameters
+    setInnerParameters_c.argtypes = [ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_int]
+
+    setInnerParameters_c.restype = ctypes.c_int
+    return setInnerParameters_c(K, Ti, Tr, beta, H, integratorOn)
+
+def getInnerParameters():
+    """
+    Gets parameters for inner controller.
+
+    Args:
+
+    Returns:
+        double: K
+        double: Ti
+        double: Tr
+        double: beta
+        double: H
+        int: integratorOn. 1 if integrator is on or 0 if not (needs to be int for c reasons)
+    """
+    getInnerParameters_c = comms_lib.getInnerParameters
+    getInnerParameters_c.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_int)]
+    getInnerParameters_c.restype = ctypes.c_int
+
+    K = ctypes.c_double()
+    Ti = ctypes.c_double()
+    Tr = ctypes.c_double()
+    beta = ctypes.c_double()
+    H = ctypes.c_double()
+    integratorOn = ctypes.c_int()
+
+    getInnerParameters_c(ctypes.byref(K), ctypes.byref(Ti), ctypes.byref(Tr), ctypes.byref(beta), ctypes.byref(H), ctypes.byref(integratorOn))
+    return K.value, Ti.value, Tr.value, beta.value, H.value, integratorOn.value
+
+def setOuterParameters(K, Ti, Td, Tr, N, beta, H, integratorOn):
+    """
+    Sets parameters for outer controller.
+
+    Args:
+        K (double): K
+        Ti (double): Ti
+        Td (double): Td
+        Tr (double): Tr
+        N (double): N
+        beta (double): beta
+        H (double): H
+        integratorOn (int): 1 if integrator is on or 0 if not (needs to be int for c reasons)
+
+        Returns:
+        int: 0 if sucessfull, non-0 of not 
+    """
+    setOuterParameters_c = comms_lib.setOuterParameters
+    setOuterParameters_c.argtypes = [ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_int]
+
+    setOuterParameters_c.restype = ctypes.c_int
+    return setOuterParameters_c(K, Ti, Td, Tr, N, beta, H, integratorOn)
+
+def getOuterParameters():
+    """
+    Gets parameters for outer controller.
+
+    Args:
+
+    Returns:
+        double: K
+        double: Ti
+        double: Td
+        double: Tr
+        double: N
+        double: beta
+        double: H
+        int: integratorOn. 1 if integrator is on or 0 if not (needs to be int for c reasons)
+    """
+
+    getOuterParameters_c = comms_lib.getOuterParameters
+    getOuterParameters_c.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_int)]
+    getOuterParameters_c.restype = ctypes.c_int
+
+    K = ctypes.c_double()
+    Ti = ctypes.c_double()
+    Td = ctypes.c_double()
+    Tr = ctypes.c_double()
+    N = ctypes.c_double()
+    beta = ctypes.c_double()
+    H = ctypes.c_double()
+    integratorOn = ctypes.c_int()
+
+    getOuterParameters_c(ctypes.byref(K), ctypes.byref(Ti), ctypes.byref(Td), ctypes.byref(Tr), ctypes.byref(N), ctypes.byref(beta), ctypes.byref(H), ctypes.byref(integratorOn))
+    return K.value, Ti.value, Td.value, Tr.value, N.value, beta.value, H.value, integratorOn.value
+
 def setMode(mode):
-    path = "./comms/setMode"
-    data = [mode]
-    write(path, data)
+    """
+    Sets Mode for regulation. OFF = 0, BEAM = 1, BALL = 2
+
+    Args:
+        mode (int): mode for regulation. OFF = 0, BEAM = 1, BALL = 2
+
+    Returns:
+        int: 0 if sucessfull, non-0 of not 
+    """
+    setMode_c = comms_lib.setMode
+    setMode_c.argtypes = [ctypes.c_int]
+
+    setMode_c.restype = ctypes.c_int
+    return setMode_c(mode)
 
 def setRef(newRef):
-    path = "./comms/setRef"
-    data = [str(newRef)]
-    write(path, data)
+    """
+    Sets reference value for regulation
+
+    Args:
+        newRef (double): New reference value
+
+    Returns:
+        int: 0 if sucessfull, non-0 of not 
+    """
+    setRef_c = comms_lib.setRef
+    setRef_c.argtypes = [ctypes.double]
+
+    setRef_c.restype = ctypes.c_int
+    return setRef_c(newRef)
 
 def shutDown():
-    path = "./comms/shutDown"
-    data = []
-    write(path, data)
+    """
+    Does nothing yet
 
-def write(path, data):
-    proc = subprocess.Popen([path], stdin=subprocess.PIPE)
-    for send in data:
-        send_str = str(send) + ' '
-        print(send_str)
-        proc.stdin.write(send_str.encode())
-    
-    proc.stdin.close()
-    proc.wait()
-    proc.terminate()
+    Args:
+
+    Returns:
+        int: 0 if sucessfull, non-0 of not 
+    """
+
+    shutDown_c = comms_lib.shutDown
+    shutDown_c.argtypes = []
+
+    shutDown_c.restype = ctypes.c_int
+    return shutDown_c()
+
+def getControlData():
+    """
+    Gets control data.
+
+    Args:
+
+    Returns:
+        double: x value
+        double: u value
+    """
+    getControlData_c = comms_lib.getControlData
+    getControlData_c.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double)]
+    getControlData_c.restype = ctypes.c_int
+
+    x = ctypes.c_double()
+    u = ctypes.c_double()
+
+    getControlData_c(ctypes.byref(x), ctypes.byref(u))
+    return x.value, u.value
+
+def getMeasurementData():
+    """
+    Gets measurement data.
+
+    Args:
+
+    Returns:
+        double: x value
+        double: yRef value
+        double: y value
+    """
+    getMeasurementData_c = comms_lib.getMeasurementData
+    getMeasurementData_c.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double)]
+    getMeasurementData_c.restype = ctypes.c_int
+
+    x = ctypes.c_double()
+    yRef = ctypes.c_double()
+    y = ctypes.c_double()
+
+    getMeasurementData_c(ctypes.byref(x), ctypes.byref(yRef), ctypes.byref(y))
+    return x.value, yRef.value, y.value
