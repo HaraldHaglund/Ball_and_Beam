33feb47e22bed68c650a3477626bacf3895d2e39
simonlarsson555 <simon.larsson.555@gmail.com>
2024-04-03 11:23:30 +0200

    3d

--- include/refGenMonitor.h
+++ include/refGenMonitor.h
@@ -1,11 +1,15 @@
 #ifndef REFGENMONIOR_H
 #define REFGENMONIOR_H
 
+#include <pthread.h>
+
 //use key 'R' when accesing shared memory
 typedef struct ReferenceGenerator_t
 {
     //vad ska vara här?
     volatile double ref;
+    pthread_mutex_t mutex;
+
 }ReferenceGenerator_t;
 
 #endif
