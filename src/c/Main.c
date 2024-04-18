5d0f6c17a56c6df557203694024b3c9ea21f3b19
simonlarsson555 <simon.larsson.555@gmail.com>
2024-04-18 14:34:56 +0200

    ja

--- src/c/Main.c
+++ src/c/Main.c
@@ -1,7 +1,7 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include "../../include/DataMonitor.h"
-#include "../../include/ModeMonitor.h"
+#include "../../include/modeMonitor.h"
 #include "../../include/PI.h"
 #include "../../include/PID.h"
 #include "../../include/ReferenceGenerator.h"
