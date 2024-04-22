3cbf93a0e55d542d27713fe4db6ef53c0647ae2e
Harald <ha4384ha-s@student.lu.se>
2024-04-22 12:54:15 +0200

    U

--- src/python/comms_dum.py
+++ src/python/comms_dum.py
@@ -1,18 +1,15 @@
 import random
+
+
 def setInnerParameters(K, Ti, Tr, beta, H, integratorOn):
-    #print('Setting inner parameters')
+    # print('Setting inner parameters')
 
-    K = 1
-    Ti = 2
-    Tr = 3
-    beta = 4
-    H = 5
-    integratorOn = False
-
+    print('Set the inner parameters to ', K, Ti, Tr, beta, H, integratorOn)
     return 0
+
 
 def getInnerParameters():
-    #print('Returning inner parameters')
+    # print('Returning inner parameters')
 
     K = 1
     Ti = 2
@@ -21,22 +18,18 @@ def getInnerParameters():
     H = 5
     integratorOn = False
 
+    print('Retrieved the inner parameters ', K, Ti, Tr, beta, H, integratorOn)
     return K, Ti, Tr, beta, H, integratorOn
 
+
 def setOuterParameters(K, Ti, Td, Tr, N, beta, H, integratorOn):
-    #print('Setting outer parameters')
-
-    K = 1
-    Ti = 2
-    Tr = 3
-    beta = 4
-    H = 5
-    integratorOn = False
-
+    # print('Setting outer parameters')
+    print('Set the outer parameters to ', K, Ti, Td, Tr, N, beta, H, integratorOn)
     return 0
 
+
 def getOuterParameters():
-    #print('Returning outer parameters')
+    # print('Returning outer parameters')
 
     K = 1
     Ti = 2
@@ -47,29 +40,35 @@ def getOuterParameters():
     H = 5
     integratorOn = False
 
+    print('Retrieved the outer parameters ', K, Ti, Td, Tr, N, beta, H, integratorOn)
     return K, Ti, Td, Tr, N, beta, H, integratorOn
 
+
 def setMode(mode):
-    #print('Mode set to ', mode)
+    print('Mode set to ', mode)
     return 0
+
 
 def setRef(newRef):
-    #print('Reference set to ', newRef)
+    # print('Reference set to ', newRef)
     return 0
+
 
 def shutDown():
-    #print('Shutting down')
+    print('Shutting down')
     return 0
 
+
 def getControlData():
-    #print('Returning control data')
+    # print('Returning control data')
 
     x = 1
     u = random.uniform(-10, 10)
     return x, u
 
+
 def getMeasurementData():
-    #print('Returning measurement data')
+    # print('Returning measurement data')
 
     x = 1
     yRef = 2
